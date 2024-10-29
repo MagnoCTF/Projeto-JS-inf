5b015eb6436d753cefb556905a3ff0eba4ba960c
Carlos Magno <84047657+MagnoCTF@users.noreply.github.com>
2024-10-29 20:11:30 -0300

    primeiro commit

---
+++ js.js
@@ -0,0 +1,9 @@
+function vazio(){
+
+    document.getElementById("carrinho").setAttribute("src","imagens/car2.png");
+}
+
+function cheio(){
+
+    document.getElementById("carrinho").setAttribute("src","imagens/car1.png");
+}
